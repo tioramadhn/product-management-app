614d9f44ce7fc8d9a0afe6a5479b77e70e4f04cb
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2024-11-10 22:08:22 +0700

    feat: sticky navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar() {
   };
 
   return (
-    <nav className="bg-primary">
+    <nav className="bg-primary sticky top-0 z-10">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
